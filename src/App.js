24d75be0fb6d3a7e8fcea5afa2014e594a4a0025
Celecis <vanessa.jv.torres@gmail.com>
2021-08-29 12:30:27 +0100

    finished sign in handleSubmit method

--- src/App.js
+++ src/App.js
@@ -26,15 +26,12 @@ class App extends React.Component {
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot((snapShot) => {
-          this.setState(
-            {
-              currentUser: {
-                id: snapShot.id,
-                ...snapShot.data(),
-              },
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data(),
             },
-            () => console.log(this.state)
-          );
+          });
         });
       }
 
