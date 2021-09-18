65be1120039a7fb679faa96c996babb1cdac39c9
Celecis <vanessa.jv.torres@gmail.com>
2021-09-18 04:25:43 +0100

    updating our app with reselect, replaced cart selectors with memoized selectors

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -2,15 +2,16 @@ import React from "react";
 import { connect } from "react-redux";
 
 import { toggleCartHidden } from "../../redux/cart/cart.actions";
+import { selectCartItemsCount } from "../../redux/cart/cart.selectors";
 
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 
 import "./cart-icon.styles.scss";
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
   <div className="cart-icon" onClick={toggleCartHidden}>
     <ShoppingIcon className="shopping-icon" />
-    <span className="item-count">0</span>
+    <span className="item-count">{itemCount}</span>
   </div>
 );
 
@@ -18,4 +19,18 @@ const mapDispatchToProps = (dispatch) => ({
   toggleCartHidden: () => dispatch(toggleCartHidden()),
 });
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+const mapStateToProps = (state) => ({
+  itemCount: selectCartItemsCount(state),
+});
+
+// THIS IS USING A SELECTOR
+// we are passing our whole reducer state to selectCartItems() selector that will reference selectCart
+// selectCart then passes the state and gets the cart obj from it
+// which will then be passed into selectCartItems, which will then pass
+// the cartItems from the cart obj TO the selectCartItemsCount selector
+// which will finnally reduce over and gives us our final cart items count
+// that wil then be apointed to itemCount inside mapStateToProps
+// that we can then use it (mapStateToProps) to pass itemCount as a prop
+// to our CartIcon component
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
---
+++ src/redux/cart/cart.selectors.js
@@ -0,0 +1,48 @@
+import { createSelector } from "reselect";
+
+// 1: INPUT SELECTOR:
+const selectCart = (state) => state.cart;
+
+// 2: OUTPUT SELECTOR:
+export const selectCartItems = createSelector(
+  [selectCart],
+  (cart) => cart.cartItems
+);
+
+// 2: OUTPUT SELECTOR:
+export const selectCartItemsCount = createSelector(
+  [selectCartItems],
+  (cartItems) =>
+    cartItems.reduce(
+      (accumalatedQuantity, cartItem) =>
+        accumalatedQuantity + cartItem.quantity,
+      0
+    )
+);
+
+// THERE ARE 2 TYPES OF SELECTORS:
+// 1: INPUT SELECTOR: doesn't use createSelector from reselect
+// 2: OUTPUT SELECTOR: uses INPUT SELECTORs and createSelector from reselect
+// for good practice, SELECTORS should always start with the keyword "select"
+
+// 1: INPUT SELECTOR:
+// it is a function that gets the whole state and returns a slice of it
+// only layer deep usually
+
+// 2: OUTPUT SELECTOR:
+// it a function that uses our createSelector() call
+// it takes up 2 arguments:
+// 1st: is a collection - an array - of INPUT SELECTORs
+// 2nd: is a function, that will return the value we want out of the selector
+// what we are going to use in it's parameters is actually each output of the input selectors in the array
+// by the order they were written
+// (in this case, our selectCart output is our cart obj, it is that cart obj that we need to put as a parameter)
+
+//
+// selectCart passes the state and gets the cart obj from it
+// which will then be passed into selectCartItems, which will then pass
+// the cartItems from the cart obj TO the selectCartItemsCount selector
+// which will finnally reduce over and gives us our final cart items count
+// that wil then be apointed to itemCount inside mapStateToProps
+// that we can then use it (mapStateToProps) to pass itemCount as a prop
+// to our CartIcon component
